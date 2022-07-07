c3982321c84e1dcb7ec6245b6956dd68024c329f
Dubjay18 <jejeniyi7@gmail.com>
2022-07-07 09:44:31 +0100

    fix:changed colors and added correct pics

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="mt-28 py-1 bg-blue-500 opacity-60 overflow-y-hidden overflow-x-hidden">
+    <div className="mt-28 h-full py-1 bg-blue-500 opacity-60 overflow-y-hidden overflow-x-hidden">
       <div className="flex items-center justify-center  font-poppins text-gray-900 opacity-100">
         Built by{" "}
         <a
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="h-[10vh] flex items-center justify-between p-5">
       <div>
-        <img src={dlogo.src} alt="" />
+        <img src={dlogo.src} alt="" className="w-32" />
       </div>
       <div>
         <span className="btn bg-blue-900 text-white border-none px-5">
